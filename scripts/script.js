ea6391e1173aa0994ec7f6290a49ab2ee85f77a6
Pickle-Pee <kirillkas5642013@gmail.com>
2020-12-17 06:54:13 +0300

    адаптив, правки 17.12

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
   $('#js-carousel-1').owlCarousel({
     items: 5,
     loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
     responsive: {
       900: {
         items: 3
@@ -14,8 +12,6 @@ $(document).ready(function(){
   $('#js-carousel-2').owlCarousel({
     items: 5,
     loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
     responsive: {
       900: {
         items: 3
